8b35a95070c45c26b75cdee63028656d8b4eb0ce
jabalnurit <jabalnur.205025@mhs.its.ac.id>
2022-10-24 04:38:10 +0700

    tambah console.log

--- app.js
+++ app.js
@@ -7,4 +7,9 @@ app.use('/three/', express.static(path.join(__dirname, 'node_modules/three')));
 app.use('/cannon-es/', express.static(path.join(__dirname, 'node_modules/cannon-es')));
 app.use('/cannon-es-debugger/', express.static(path.join(__dirname, 'node_modules/cannon-es-debugger')));
 
-app.listen(3000);
+const port = 3000;
+const host = 'localhost';
+
+app.listen(port, host, () => {
+    console.log(`App berjalan pada http://${host}:${port}`);
+});
